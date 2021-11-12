9bff6f656ec5f418e666d44c05aac15f885c375c
Dainobu <dianevu@hotmail.ca>
2021-11-12 09:54:37 -0500

    1 file modified - Added couple tests

--- src/app/mortgage-calculator-form/mortgage-calculator-form.component.spec.ts
+++ src/app/mortgage-calculator-form/mortgage-calculator-form.component.spec.ts
@@ -1,4 +1,35 @@
-describe('Testing tests', () => {
-  it('should succeed', () => expect(true).toEqual(true));
-  //it('should fail', () => expect(true).toEqual(false));
+import { CurrencyPipe } from '@angular/common';
+import { Terms } from '../terms.enum';
+import { MortgageCalculatorFormComponent } from './mortgage-calculator-form.component';
+
+describe('Testing form functions', () => {
+  var form;
+  //This will be called before running each spec
+  beforeEach(function () {
+    form = new MortgageCalculatorFormComponent();
+  });
+
+  describe('Testing getKeys function', () => {
+    it('should give term enum keys', () =>
+      expect(form.getKeys(Terms)).toEqual([
+        '1',
+        '2',
+        '3',
+        '4',
+        '5',
+        '6',
+        '7',
+        '8',
+        '9',
+        '10',
+      ]));
+  });
+  describe('Testing calculateMortgagePayment function', () => {
+    it('should calculate fixed periodic payment', () =>
+      expect(
+        +form
+          .calculateMortgagePayment(100000, 0.004167, 1.28336, 0.28336)
+          .toFixed(2)
+      ).toEqual(1887.27));
+  });
 });
